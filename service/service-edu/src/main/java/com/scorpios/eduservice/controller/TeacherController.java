b3ba112167eb46a1cb6d68d765fba676bb883254
scorpios <1435513775@qq.com>
2020-03-29 16:53:38 +0800

    接口添加swagger注解，完善接口说明

--- service/service-edu/src/main/java/com/scorpios/eduservice/controller/TeacherController.java
+++ service/service-edu/src/main/java/com/scorpios/eduservice/controller/TeacherController.java
@@ -4,6 +4,9 @@ package com.scorpios.eduservice.controller;
 import com.scorpios.common.utils.CommonResponse;
 import com.scorpios.eduservice.entity.Teacher;
 import com.scorpios.eduservice.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +16,7 @@ import java.util.List;
  * @author scorpios
  * @since 2020-03-29
  */
+@Api(description = "讲师管理")
 @RestController
 @RequestMapping("/eduService/teacher")
 public class TeacherController {
@@ -20,14 +24,18 @@ public class TeacherController {
     @Autowired
     TeacherService teacherService;
 
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("/all")
     public CommonResponse findAll(){
         List result = teacherService.list(null);
         return CommonResponse.ok().data("list",result);
     }
 
+    @ApiOperation(value = "根据ID删除讲师")
     @DeleteMapping("/{id}")
-    public CommonResponse removeById(@PathVariable String id){
+    public CommonResponse removeById(
+            @ApiParam(name = "id", value = "讲师ID", required = true)
+            @PathVariable String id){
         return CommonResponse.ok().data("result",true);
     }
 
