7815ad0477d8524761c6dbe8bca8a134850fde6b
scorpios <1435513775@qq.com>
2020-03-29 13:30:37 +0800

    添加swagger

--- service/service-edu/src/main/java/com/scorpios/eduservice/controller/TeacherController.java
+++ service/service-edu/src/main/java/com/scorpios/eduservice/controller/TeacherController.java
@@ -4,10 +4,7 @@ package com.scorpios.eduservice.controller;
 import com.scorpios.eduservice.entity.Teacher;
 import com.scorpios.eduservice.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,11 +19,15 @@ public class TeacherController {
     @Autowired
     TeacherService teacherService;
 
-
     @GetMapping("/all")
     public List<Teacher> findAll(){
         List result = teacherService.list(null);
         return result;
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean removeById(@PathVariable String id){
+        return teacherService.removeById(id);
     }
 
 }
